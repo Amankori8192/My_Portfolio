46dbc1e031fde5b6fe4d4b035e70a223c5274c22
Aman_kori <amankori2025@gmail.com>
2025-06-30 18:15:17 +0530

    files upload

---
+++ script.js
@@ -0,0 +1,69 @@
+// Disable right-click context menu
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+});
+
+
+// const submitbtn = document.querySelector("#sendBtn");
+
+// submitbtn.addEventListener("click", function () {
+//   alert("Message sent!");
+// });
+
+
+
+
+
+
+// gsap.registerPlugin(SplitText) ;
+
+// SplitText.create(".split", {
+//   type: "lines, words",
+//   mask: "lines",
+//   autoSplit: true,
+//   onSplit(self) {
+//     return gsap.from(self.words, {
+//       duration: 1, 
+//       y: 100, 
+//       autoAlpha: 0, 
+//       stagger: 0.05
+//     });
+//   }
+// });
+
+
+
+    // //   // GSAP animation for the hero section
+    // //   gsap.from(".hero-section .lines h2", {
+    // //     duration: 1,
+    // //     y: -50,
+    // //     opacity: 0,
+    // //     stagger: 0.3,
+    // //     ease: "power2.out"
+    // //   });
+
+    //   gsap.from(".hero-section .lines .line2", {
+    //     duration: 1,
+    //     y: -50,
+    //     opacity: 0,
+    //     stagger: 0.3,
+    //     delay: 0.5,
+    //     ease: "power2.out"
+    //   });
+
+    // //   gsap.from(".hero-section .social-icons img", {
+    // //     duration: 1,
+    // //     y: -50,
+    // //     opacity: 0,
+    // //     stagger: 0.3,
+    // //     delay: 1,
+    // //     ease: "power2.out"
+    // //   });
+
+    // //     gsap.from(".hero-section .btn", {
+    // //         duration: 1,
+    // //         y: -50,
+    // //         opacity: 0,
+    // //         delay: 1.5,
+    // //         ease: "power2.out"
+    // //     });
